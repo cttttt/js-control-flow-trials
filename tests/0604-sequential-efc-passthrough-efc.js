424aad17899754ad6039525b8ad707929d388021
Chris Taylor <ctaylorr@gmail.com>
2017-04-21 18:51:42 -0400

    ct: Fix up a test.

--- tests/0604-sequential-efc-passthrough-efc.js
+++ tests/0604-sequential-efc-passthrough-efc.js
@@ -3,9 +3,9 @@ var tap = require('tap'),
     passthrough = require('../trials/06-sequential-efc').passthrough;
 
 var random = Math.floor(Math.random() * 10);
-var stub = sinon.stub().yields(random);
+var stub = sinon.stub().yields(null, random);
 var callback = sinon.stub();
 passthrough(stub, callback);
-tap.equals(stub.callCount, 1, 'Was the callback called exactly once?');
+tap.equals(stub.callCount, 1, 'Was the random value returning function called exactly once?');
 tap.equals(callback.callCount, 1, 'Was the callback called exactly once?');
-tap.equals(callback.args[0][1], random, 'Was the callback called with the provided value?');
+tap.equals(callback.firstCall.args[1], random, 'Was the callback called with the provided value?');
