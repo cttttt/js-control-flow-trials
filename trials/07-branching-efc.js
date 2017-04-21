8925820e4daeed3465ac98fad828107213b04b82
Chris Taylor <ctaylorr@gmail.com>
2017-04-21 18:53:52 -0400

    ct: Clarify an explanation.

--- trials/07-branching-efc.js
+++ trials/07-branching-efc.js
@@ -1,3 +1,7 @@
-// Create an
+// Create an error first callback function that takes an error first
+// callback function and a callback.  Have your function call first
+// provided function and "return" "True" or "False", depending on what
+// it "return".
+//
 module.exports.boolToString = function (getNextBool, callback) {
 };
