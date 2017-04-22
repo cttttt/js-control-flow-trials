2635bc4139be1d3cbbadde77506eedd40543f892
Chris Taylor <ctaylorr@gmail.com>
2017-04-22 16:54:12 -0400

    ct: Improve documentation for trials.

--- trials/14-map-promises.js
+++ trials/14-map-promises.js
@@ -1,22 +1,22 @@
-// Create an error-first-callback function that takes a list, a transform
-// function and a callback function.  "Return" (through the callback) an array
-// with elements each created by running the transform function on the original
-// list's elements.
+// Create an error-first-callback function that takes a list, a
+// transform function and a callback function.  "Return" (through the
+// callback) an array with elements each created by running the
+// transform function on the original list's elements.
 //
-// Do not use async.map.
+// Do not use Bluebird's Promise.map().
 //
 //
 // THIS IS DIFFICULT!!!!
 module.exports.transformer = function (list, transform) {
 }
 
-// Create an error-first-callback function that takes a list, a transform
-// function and a callback function.  "Return" (through the callback) an array
-// with elements each created by running the transform function on the original
-// list's elements.
+// Create an error-first-callback function that takes a list, a
+// transform function and a callback function.  "Return" an array with
+// elements each created by running the transform function on the
+// original list's elements.
 //
-// Here, feel free to use async.map
+// Here, feel free to use Bluebird's Promise.map().
 //
 var Promise = require('bluebird');
-module.exports.transformerBluebirdMap = function (list, transform, callback) {
+module.exports.transformerBluebirdMap = function (list, transform) {
 }
