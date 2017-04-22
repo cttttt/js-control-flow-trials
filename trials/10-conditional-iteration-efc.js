d417bf71cc1dab7fd719b39f22eda06b0b405610
Chris Taylor <ctaylorr@gmail.com>
2017-04-22 14:51:44 -0400

    ct: Improve the explanation.

--- trials/10-conditional-iteration-efc.js
+++ trials/10-conditional-iteration-efc.js
@@ -1,2 +1,12 @@
+// Write an error-first-callback function that accepts two
+// error-first-callback functions and a callback.  Repeatedly call
+// the first one, and every time it returns something truthy, call
+// the second.  When the first one returns something falsey,
+// "return" by calling the callback.
+//
+// i.e. Implement trials/05-conditional-iteration-synchronous.js as
+// an error-first-callback function that accepts
+// error-first-callback functions.
+//
 module.exports.poller = function poller(areWeThere, nope, callback) {
 };
