546681711982243ffbdb913297d9b16a92f6b084
Chris Taylor <ctaylorr@gmail.com>
2017-04-22 15:54:03 -0400

    ct: Better explain a trial.

--- trials/12-branching-promises.js
+++ trials/12-branching-promises.js
@@ -1,2 +1,7 @@
+// Write a promise-returning function that accepts a promise retruning
+// function, getNextBool. getNextBool() will "return" a random boolean
+// value.  Retrieve it and "return" the string, "True" or "False"
+// depending on the truthiness of the value.
+//
 module.exports.boolToString = function (getNextBool) {
 };
